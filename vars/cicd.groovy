0b7a02ea002244fc4f847254b3f28e98c2f2f471
devopsakhil5 <110562970+devopsakhil5@users.noreply.github.com>
2022-08-17 23:23:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -3,7 +3,15 @@ def down(repo)
  git "${repo}"
 }
 
+
+
 def build()
 {
  sh 'mvn package'
+}
+
+
+def deploy(jobname, ip, appname)
+{
+ scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@{ip}:/var/lib/tomcat9/webapp/${appname}
 }
